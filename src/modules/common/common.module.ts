67e5708c176557730c890a0694f974858cae1c8f
Al Shahriar Niloy <shahriar_niloy@outlook.com>
2025-09-24 22:12:06 +1000

    Use config service for DB initialization

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "root",
-    database: "markopolo",
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST', 'localhost'),
+        port: configService.get<number>('DATABASE_PORT', 5432),
+        username: configService.get<string>('DATABASE_USERNAME', 'user'),
+        password: configService.get<string>('DATABASE_PASSWORD', 'dummypassword'),
+        database: configService.get<string>('DATABASE_NAME', 'markopolodb'),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
